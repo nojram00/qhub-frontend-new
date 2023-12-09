d1bf2d040d868bcfdb59076dd79214a3651c28c7
nojram00 <113487670+nojram00@users.noreply.github.com>
2023-12-09 22:23:25 +0800

    ok na to dapat

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import QuotationPage from './components/quotationPage';
+import Home from './components/homepage'
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+// import "./app.scss";
+import { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios'
+import Header from './partials/_header';
+import http from './axios_config/axios.config';
+import Login from './components/login';
+
 
 function App() {
+
+  const getUser = async () => {
+
+    await http.get('/sanctum/csrf-cookie').then((csrf) => {
+      console.log('csrf: ', csrf);
+    }).catch((err) => {
+      console.error(err);
+    });
+
+
+    await http.get('/api/user', {
+
+    }).then((user) => {
+      console.log("user: ", user);
+    }).catch((err) => {
+      console.log(err);
+    });
+
+  }
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PrimeReactProvider>
+      <BrowserRouter>
+        <Header />
+          <Routes>
+            <Route element={<QuotationPage />} path='/quote'/>
+            <Route element={<Home />} path='/'/>
+            <Route element={<Login />} path='/login'/>
+          </Routes>
+      </BrowserRouter>
+    </PrimeReactProvider>
   );
 }
 
